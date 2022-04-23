87b8fc9667a6c0e5f69349206f86f217901703e4
adalbertobrant <adalbertobrant@gmail.com>
2022-04-23 15:02:30 -0300

    continuando a transformar a main.py para uma classe em window.py

--- mineSwiper/window.py
+++ mineSwiper/window.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 import tkinter as tk
 import settings as ss
 import utils 
@@ -12,8 +12,29 @@ class Window(tk.Tk):
     self.geometry(f"{ss.WIDTH}x{ss.HEIGHT}")
     self.resizable(False,False)
     self.title("Mine Sweeper")
+    # Frame creation 
+    top_frame = Frame(
+      self,
+      bg='green',
+      width=ss.WIDTH,
+      height=utils.height_prct(25)
+    )
+    top_frame.place(x=0, y=0)
 
-  
+    game_title = Label(
+      top_frame, 
+      bg='black',
+      fg='white',
+      text='MineSweeper Game, by Adalberto Filho',
+      font=('', 30),
+    )
+
+    game_title.place(
+      x=utils.width_prct(25),
+      y=utils.height_prct(7),
+    )
+
+    
 # área principal do programa
 
 if __name__ == "__main__":
